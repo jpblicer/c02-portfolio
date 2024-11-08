5c47eb2ad40b1ff1e3aa5f7a3cebb75851727727
James Young <jpblicer@gmail.com>
2024-11-08 11:12:29 +0900

    video always in view

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,43 +1,43 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const tabs = document.querySelectorAll('.main-nav-link');
-    const tabContent = document.getElementById('pills-tabContent');
-    const videoBackground = document.createElement('video');
-    
-    // Fix the video to the viewport
-    videoBackground.style.position = 'fixed';
-    videoBackground.style.top = 0;
-    videoBackground.style.left = 0;
-    videoBackground.style.width = '100vw';  
-    videoBackground.style.height = '100vh'; 
-    videoBackground.style.objectFit = 'cover'; 
-    videoBackground.style.zIndex = '-1'; 
-    videoBackground.muted = true;
-    videoBackground.loop = true;
-    tabContent.appendChild(videoBackground);
+	const tabs = document.querySelectorAll('.main-nav-link');
+	const tabContent = document.getElementById('pills-tabContent');
+	const videoBackground = document.createElement('video');
 
-    // Set the initial video
-    videoBackground.src = '../images/a.mp4';
-    videoBackground.play();
+	// Fix the video to the viewport
+	videoBackground.style.position = 'fixed';
+	videoBackground.style.top = 0;
+	videoBackground.style.left = 0;
+	videoBackground.style.width = '100vw';
+	videoBackground.style.height = '100vh';
+	videoBackground.style.objectFit = 'cover';
+	videoBackground.style.zIndex = '-1';
+	videoBackground.muted = true;
+	videoBackground.loop = true;
+	tabContent.appendChild(videoBackground);
 
-    // Change video source based on tab clicked
-    tabs.forEach(tab => {
-        tab.addEventListener('click', function() {
-            videoBackground.src = '';
+	// Set the initial video
+	videoBackground.src = '../images/a.mp4';
+	videoBackground.play();
 
-            if (this.id === 'pills-home-tab') {
-                videoBackground.src = '../images/a.mp4';
-            } else if (this.id === 'pills-about-tab') {
-                videoBackground.src = '../images/b.mp4';
-            } else if (this.id === 'pills-projects-tab') {
-                videoBackground.src = '../images/c.mp4';
-            } else if (this.id === 'pills-services-tab') {
-                videoBackground.src = '../images/d.mp4';
-            }
+	// Change video source based on tab clicked
+	tabs.forEach(tab => {
+		tab.addEventListener('click', function() {
+			videoBackground.src = '';
 
-            videoBackground.load();
-            videoBackground.play();
-        });
-    });
+			if (this.id === 'pills-home-tab') {
+				videoBackground.src = '../images/a.mp4';
+			} else if (this.id === 'pills-about-tab') {
+				videoBackground.src = '../images/b.mp4';
+			} else if (this.id === 'pills-projects-tab') {
+				videoBackground.src = '../images/c.mp4';
+			} else if (this.id === 'pills-services-tab') {
+				videoBackground.src = '../images/d.mp4';
+			}
+
+			videoBackground.load();
+			videoBackground.play();
+		});
+	});
 });
 
 
@@ -52,33 +52,33 @@ function showAbout() {
 
 
 document.addEventListener("DOMContentLoaded", function() {
-    const largeProjectContainer = document.getElementById("large-project-container");
+	const largeProjectContainer = document.getElementById("large-project-container");
 
-    const overlays = [
-        document.getElementById("large-project-container-overlay-a"),
-        document.getElementById("large-project-container-overlay-b"),
-        document.getElementById("large-project-container-overlay-c"),
-        document.getElementById("large-project-container-overlay-d")
-    ];
+	const overlays = [
+		document.getElementById("large-project-container-overlay-a"),
+		document.getElementById("large-project-container-overlay-b"),
+		document.getElementById("large-project-container-overlay-c"),
+		document.getElementById("large-project-container-overlay-d")
+	];
 
-    const smallProjectContainers = [
-        document.getElementById("small-project-container-a"),
-        document.getElementById("small-project-container-b"),
-        document.getElementById("small-project-container-c"),
-        document.getElementById("small-project-container-d")
-    ];
+	const smallProjectContainers = [
+		document.getElementById("small-project-container-a"),
+		document.getElementById("small-project-container-b"),
+		document.getElementById("small-project-container-c"),
+		document.getElementById("small-project-container-d")
+	];
 
-    const smallProjectsContainerOverlay = document.getElementById("small-projects-container-overlay");
+	const smallProjectsContainerOverlay = document.getElementById("small-projects-container-overlay");
 
-    smallProjectContainers.forEach((container, index) => {
-        container.addEventListener("click", () => {
-            overlays.forEach(overlay => overlay.classList.add("hidden"));
-            overlays[index].classList.toggle("hidden");
-        });
-    });
+	smallProjectContainers.forEach((container, index) => {
+		container.addEventListener("click", () => {
+			overlays.forEach(overlay => overlay.classList.add("hidden"));
+			overlays[index].classList.toggle("hidden");
+		});
+	});
 
-    largeProjectContainer.addEventListener('click', () => {
-        smallProjectsContainerOverlay.classList.toggle("hidden");
-    });
+	largeProjectContainer.addEventListener('click', () => {
+		smallProjectsContainerOverlay.classList.toggle("hidden");
+	});
 });
 
